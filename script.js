dcebdcbbab3cfc62359c788ab4788efc31396fc2
MalakhN <106473648+MalakhN@users.noreply.github.com>
2022-11-06 18:51:24 +0300

    Mesto Russia 2

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 let formElement = document.querySelector('.form');
 let nameInput = document.querySelector('#name');
 let jobInput = document.querySelector('#description');
-let closeButton = document.querySelector('.close-button');
+let closeButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let noLike = document.querySelector('.element__icon-image');
+let Like = document.querySelector('.element__icon-image_like');
 
 // Функция переключения попапа
 function togglePopup () {
   popup.classList.toggle('popup_opened');
+  formElement.reset();
 }
 
 // Функция отправки
@@ -15,8 +20,6 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     nameInput.getAttribute('value');
     jobInput.getAttribute('value');
-    let profileName = document.querySelector('.profile__name');
-    let profileDescription = document.querySelector('.profile__description');
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
     togglePopup();
